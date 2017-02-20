5ab3fcca7873df64531c5750fd5207e696a37898
lionder <lionder.97@bk.ru>
2017-02-21 01:27:19 +0200

    Added function
    Added function "get_repo_info".

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import pprint
 import json
 import requests
 from requests import exceptions
+import getpass
 
 
 def get_repos():
@@ -12,27 +13,53 @@ def get_repos():
     else:
         print(response.status_code)
         print(response.content)
-        print(response.json())
         pprint.pprint(response.json())
     finally:
-        print("passed")
+        print("get_repos finished!")
 
 
 def create_repo():
     username = input('Username: ')
-    password = input('Password: ')
+    password = getpass.getpass('Password: ')
     repodata = {'name': input('Repository Name: ')}
     try:
-        response = requests.post('https://api.github.com/user/repos', auth=(username, password), data=json.dumps(repodata))
+        response = requests.post('https://api.github.com/user/repos', auth=(username, password),
+                                 data=json.dumps(repodata))
 
     except exceptions.InvalidSchema as e:
         print(e)
     else:
         print(response.status_code)
         print(response.content)
-        print(response.json())
         pprint.pprint(response.json())
     finally:
-        print("passed")
+        print("create_repo finished!")
 
 
+def get_repo_info():
+    username = input('Username: ')
+    repo = input('Repository Name: ')
+    try:
+        response = requests.get('https://api.github.com/repos/%s/%s' % (username, repo))
+    except exceptions.InvalidSchema as e:
+        print(e)
+    else:
+        print(response.status_code)
+        repo_obj = response.json()
+        for param, value in repo_obj.items():
+            if not isinstance(value, dict):
+                print(param + " = " + str(value))
+            else:
+                print(param + " = complicated object")
+            if param.endswith("_url") and value is not None and value != "https://github.com/%s/%s" % (username, repo) and not value.endswith(".git"):
+                try:
+                    response2 = requests.get(value)
+                except (exceptions.InvalidSchema, exceptions.MissingSchema) as e:
+                    print(e)
+                else:
+                    print(".....count: " + str(len(response2.json())))
+    finally:
+        print("get_repo_info finished!")
+
+
+get_repo_info()
